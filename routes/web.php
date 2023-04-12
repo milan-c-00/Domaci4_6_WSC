b2dafdb219fdaf8b066adfa5a97e2f9f839800f8
milan-c-00 <cvijovic.m21@gmail.com>
2023-04-12 14:10:41 +0200

    Minor change in web.php, restricted profile route for users

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware(['auth', 'role:admin'])->group(function() {
 
     Route::resource('users', UserController::class);
 
 });
+//
+//Route::middleware(['auth', 'role:admin'])->group(function() {
+//
+//    Route::resource('users', UserController::class);
+//
+//});
 
 require __DIR__.'/auth.php';
